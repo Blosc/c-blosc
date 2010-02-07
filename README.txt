9ba48dafedd639b089faa42b7dee0f1dcff45b20
FrancescAlted <faltet@gmail.com>
2010-02-07 19:49:36 +0000

    The small benchmark in blosc.c has been moved into its own bench.c file.
    git-svn-id: http://blosc.pytables.org/svn/blosc/trunk@29 5b11b6af-1e7b-4319-8082-0aacbd7ffead

--- README.txt
+++ README.txt
@@ -14,18 +14,21 @@ Blosc is distributed using the MIT license, see file LICENSES
 directory for details.
 
 Blosc consists of the next files:
-blosc.h and blosc.c    -- the main routines
-blosclz.h and blosclz.c  -- the actual compressor (based on FastLZ)
+blosc.h and blosc.c      -- the main routines
+blosclz.h and blosclz.c  -- the actual compressor
 shuffle.h and shuffle.c  -- the shuffle code
 
 Just add these files to your project in order to use Blosc. For
 information on compression and decompression routines, see blosc.h.
 
-A simple usage example is in the main function of the blosc.c file.
+Blosc is a compressor for binary data, and it gets best results if you
+can provide the size of the data type that originated the data file.
 
 To compile using GCC:
 
   gcc -O3 -msse2 -o your_program your_program.c blosc.c blosclz.c shuffle.c
+
+A simple usage example is the benchmark in the src/bench.c file.
 
 I have not tried to compile this with other compilers than GCC yet.
 Please report your experiences with your own platforms.  Thank you!
---
+++ src/bench.c
@@ -0,0 +1,121 @@
+/*********************************************************************
+  Small benchmark for testing basic capabilities of Blosc.  Please
+  report your feedback or performance figures (and your machine
+  configuration) to:
+
+  Author: Francesc Alted (faltet@pytables.org)
+
+  See LICENSES/BLOSC.txt for details about copyright and rights to use.
+**********************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <math.h>
+#include <time.h>
+
+
+/* Constants for benchmarking purposes */
+#define MB    1024*1024
+#define NITER 4*4000            /* Number of iterations */
+#define CLK_NITER CLOCKS_PER_SEC*NITER
+
+int main(void) {
+  unsigned int nbytes, cbytes;
+  void *src, *dest, *srccpy;
+  size_t i;
+  clock_t last, current;
+  float tmemcpy, tshuf, tunshuf;
+  int size = 32*1024;         /* Buffer size */
+  unsigned int elsize;                 /* Datatype size */
+  int *_src;
+  int *_srccpy;
+
+  src = malloc(size+6);
+  srccpy = malloc(size+6);
+  dest = malloc(size+6);
+
+  srand(1);
+
+  /* Initialize the original buffer */
+  _src = (int *)src;
+  _srccpy = (int *)srccpy;
+  elsize = sizeof(int);
+  for(i = 0; i < size/elsize; ++i) {
+    /* Choose one below */
+    /* _src[i] = 1; */
+    /* _src[i] = 0x01010101; */
+    /* _src[i] = 0x01020304; */
+    /* _src[i] = i * 1/.3; */
+    /* _src[i] = i; */
+    _src[i] = rand() >> 22;
+  }
+
+  /* For data coming from a file */
+  /* int fd; */
+  /* ssize_t rbytes; */
+  /* elsize = 8; */
+  /* fd = open("16KB-block-8B-typesize.data", 0); */
+  /* off_t seek = lseek(fd, 0, SEEK_SET); */
+  /* rbytes = read(fd, src, size); */
+  /* /\* rbytes = read(fd, src, 8192); *\/ */
+  /* size = rbytes; */
+  /* printf("Read %zd bytes with %zd seek\n", rbytes, seek); */
+  /* if (rbytes == -1) { */
+  /*   perror(NULL); */
+  /* } */
+  /* close(fd); */
+
+  set_complevel(1);
+  set_shuffle(1);
+
+  memcpy(srccpy, src, size);
+
+  last = clock();
+  for (i = 0; i < NITER; i++) {
+    memcpy(dest, src, size);
+  }
+  current = clock();
+  tmemcpy = (current-last)/((float)CLK_NITER);
+  printf("memcpy:\t\t %fs, %.1f MB/s\n", tmemcpy, size/(tmemcpy*MB));
+
+  last = clock();
+  for (i = 0; i < NITER; i++) {
+    cbytes = blosc_compress(elsize, size, src, dest);
+  }
+  current = clock();
+  tshuf = (current-last)/((float)CLK_NITER);
+  printf("blosc_compress:\t %fs, %.1f MB/s\t", tshuf, size/(tshuf*MB));
+  printf("Orig bytes: %d  Final bytes: %d\n", size, cbytes);
+
+  last = clock();
+  for (i = 0; i < NITER; i++)
+    if (cbytes == 0) {
+      memcpy(dest, src, size);
+      nbytes = size;
+    }
+    else {
+      nbytes = blosc_decompress(dest, src, size);
+    }
+  current = clock();
+  tunshuf = (current-last)/((float)CLK_NITER);
+  printf("blosc_d:\t %fs, %.1f MB/s\t", tunshuf, nbytes/(tunshuf*MB));
+  printf("Orig bytes: %d  Final bytes: %d\n", cbytes, nbytes);
+
+  /* Check if data has had a good roundtrip */
+  _src = (int *)src;
+  _srccpy = (int *)srccpy;
+  for(i = 0; i < size/sizeof(int); ++i){
+    if (_src[i] != _srccpy[i]) {
+      printf("Error: original data and round-trip do not match in pos %d\n",
+             (int)i);
+      printf("Orig--> %x, Copy--> %x\n", _src[i], _srccpy[i]);
+      goto out;
+    }
+  }
+
+ out:
+  free(src); free(srccpy);  free(dest);
+  return 0;
+}
