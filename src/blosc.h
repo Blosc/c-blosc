3e43c0097b9edfb8a748682b06f1f8e00cc7c849
FrancescAlted <faltet@gmail.com>
2010-06-04 14:30:36 +0000

    Version set to 0.9.4.dev.
    git-svn-id: http://blosc.pytables.org/svn/blosc/trunk@136 5b11b6af-1e7b-4319-8082-0aacbd7ffead

--- src/blosc.h
+++ src/blosc.h
@@ -13,9 +13,9 @@
 /* Version numbers */
 #define BLOSC_VERSION_MAJOR    0    /* for major interface/format changes  */
 #define BLOSC_VERSION_MINOR    9    /* for minor interface/format changes  */
-#define BLOSC_VERSION_RELEASE  3    /* for tweaks, bug-fixes, or development */
+#define BLOSC_VERSION_RELEASE  4    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "0.9.3"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "0.9.4.dev"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
 #define BLOSC_VERSION_DATE     "2010-06-04"    /* date version */
 
