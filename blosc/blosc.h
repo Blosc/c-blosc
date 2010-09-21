e280f737eb3cf7c0b2e06b43b4fe48be63a3a028
FrancescAlted <faltet@gmail.com>
2010-09-21 08:40:11 +0000

    Yet another test with Date.
    git-svn-id: http://blosc.pytables.org/svn/blosc/trunk@262 5b11b6af-1e7b-4319-8082-0aacbd7ffead

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -18,7 +18,7 @@
 
 #define BLOSC_VERSION_STRING   "1.1.b3.dev"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
-#define BLOSC_VERSION_DATE     "$Date::            $"    /* date version */
+#define BLOSC_VERSION_DATE     "$Date:: 2010-09-21$"    /* date version */
 
 /* The *_VERS_FORMAT should be just 1-byte long */
 #define BLOSC_VERSION_FORMAT    2   /* Blosc format version, starting at 1 */
