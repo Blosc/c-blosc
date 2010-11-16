a345ddf944231feca528a495f48009fba2215108
FrancescAlted <faltet@gmail.com>
2010-11-16 16:56:53 +0000

    Releasing 1.1.3.
    git-svn-id: http://blosc.pytables.org/svn/blosc/trunk@296 5b11b6af-1e7b-4319-8082-0aacbd7ffead

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -16,7 +16,7 @@
 #define BLOSC_VERSION_MINOR    1    /* for minor interface/format changes  */
 #define BLOSC_VERSION_RELEASE  3    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "1.1.3.dev"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "1.1.3"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
 #define BLOSC_VERSION_DATE     "$Date::             $"    /* date version */
 
