235cc4f405c8dc2ef4fe20edaf2435cb7fdccbbb
Valentin Haenel <valentin.haenel@gmx.de>
2012-05-03 12:03:15 +0200

    typo in blosc.h docstring

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -59,7 +59,7 @@
   between 0 (no compression) and 9 (maximum compression).
 
   `doshuffle` specifies whether the shuffle compression preconditioner
-  should be applyied or not.  0 means not applying it and 1 means
+  should be applied or not.  0 means not applying it and 1 means
   applying it.
 
   `typesize` is the number of bytes for the atomic type in binary
