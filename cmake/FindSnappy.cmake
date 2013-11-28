f07a914937e1152cbd4e3af4996cc4f883d2edce
Valentin Haenel <valentin.haenel@gmx.de>
2013-11-28 19:37:17 +0100

    conditional compile support for snappy
    This adds CMake support for conditionally compiling snappy support for blosc.
    Should still be able to copy and paste sources, since HAVE_SNAPPY is only
    defined via config.h, but will need an empty config.h... Not sure how to solve
    that one yet.

---
+++ cmake/FindSnappy.cmake
@@ -0,0 +1,17 @@
+find_path(SNAPPY_INCLUDE_DIR snappy.h NO_DEFAULT_PATH PATHS
+  /usr/include
+  /usr/local/include
+)
+
+find_library(SNAPPY_LIBRARY NAMES snappy NO_DEFAULT_PATH PATHS
+    /usr/lib
+    /usr/local/lib
+    )
+
+if (SNAPPY_INCLUDE_DIR AND SNAPPY_LIBRARY)
+    set(HAVE_SNAPPY TRUE)
+    message(STATUS "Have snappy")
+else ()
+    message(STATUS "No snappy found")
+endif ()
+
