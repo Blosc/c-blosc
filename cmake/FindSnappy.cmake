7f1c00b072717df64190b45ac9a0631717780d7f
Francesc Alted <francesc@continuum.io>
2013-12-28 17:47:54 +0100

    Much improved support for discovery of Zlib and also Snappy

--- cmake/FindSnappy.cmake
+++ cmake/FindSnappy.cmake
@@ -1,12 +1,6 @@
-find_path(SNAPPY_INCLUDE_DIR snappy-c.h NO_DEFAULT_PATH PATHS
-  /usr/include
-  /usr/local/include
-)
+find_path(SNAPPY_INCLUDE_DIR snappy-c.h)
 
-find_library(SNAPPY_LIBRARY NAMES snappy NO_DEFAULT_PATH PATHS
-    /usr/lib
-    /usr/local/lib
-    )
+find_library(SNAPPY_LIBRARY NAMES snappy)
 
 if (SNAPPY_INCLUDE_DIR AND SNAPPY_LIBRARY)
     set(HAVE_SNAPPY TRUE)
