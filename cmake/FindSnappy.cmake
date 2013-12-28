3bd48d35502dad82dd7a1231de6df56b57130788
Francesc Alted <francesc@continuum.io>
2013-12-29 00:17:55 +0100

    Added the first fixes for snappy as an external project from Antonio

--- cmake/FindSnappy.cmake
+++ cmake/FindSnappy.cmake
@@ -2,7 +2,13 @@ find_path(SNAPPY_INCLUDE_DIR snappy-c.h)
 
 find_library(SNAPPY_LIBRARY NAMES snappy)
 
-if (SNAPPY_INCLUDE_DIR AND SNAPPY_LIBRARY)
+# handle the QUIETLY and REQUIRED arguments and set SNAPPY_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SNAPPY
+    REQUIRED_VARS SNAPPY_LIBRARY SNAPPY_INCLUDE_DIR)
+
+if(SNAPPY_FOUND)
     set(HAVE_SNAPPY TRUE)
     message(STATUS "Have snappy: ${SNAPPY_INCLUDE_DIR} ${SNAPPY_LIBRARY}")
 else ()
