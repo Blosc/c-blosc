56c89f44fc4dfad68678595a06ffff85828430d9
Francesc Alted <francesc@continuum.io>
2013-12-29 11:48:48 +0100

    Added support for compiling local sources for snappy if library not found

--- cmake/FindSnappy.cmake
+++ cmake/FindSnappy.cmake
@@ -3,9 +3,8 @@ find_path(SNAPPY_INCLUDE_DIR snappy-c.h)
 find_library(SNAPPY_LIBRARY NAMES snappy)
 
 if (SNAPPY_INCLUDE_DIR AND SNAPPY_LIBRARY)
-    set(HAVE_SNAPPY TRUE)
-    message(STATUS "Have snappy: ${SNAPPY_INCLUDE_DIR} ${SNAPPY_LIBRARY}")
+    set(SNAPPY_FOUND TRUE)
+    message(STATUS "Found snappy library: ${SNAPPY_LIBRARY}")
 else ()
-    message(STATUS "No snappy found")
+    message(STATUS "No snappy found.  Using internal sources.")
 endif ()
-
