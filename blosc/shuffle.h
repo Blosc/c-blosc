8bdd72b6b142323fbc26a446aa45e81b0bf07561
cspeller <cspeller@sidefx.com>
2014-10-20 10:28:03 -0400

    Const correctness for shuffle.

--- blosc/shuffle.h
+++ blosc/shuffle.h
@@ -10,7 +10,7 @@
 /* Shuffle/unshuffle routines */
 
 void shuffle(size_t bytesoftype, size_t blocksize,
-             unsigned char* _src, unsigned char* _dest);
+             const unsigned char* _src, unsigned char* _dest);
 
 void unshuffle(size_t bytesoftype, size_t blocksize,
-               unsigned char* _src, unsigned char* _dest);
+               const unsigned char* _src, unsigned char* _dest);
