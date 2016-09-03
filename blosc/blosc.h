1fef4fe4a1234cd088aab8ed359f9f5bba7dc480
Francesc Alted <faltet@gmail.com>
2016-09-03 07:56:06 +0200

    Getting ready for 1.11.1 release

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -21,9 +21,9 @@ extern "C" {
 #define BLOSC_VERSION_MINOR    11   /* for minor interface/format changes  */
 #define BLOSC_VERSION_RELEASE  1    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "1.11.1.dev"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "1.11.1"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
-#define BLOSC_VERSION_DATE     "$Date:: 2016-09-02 #$"    /* date version */
+#define BLOSC_VERSION_DATE     "$Date:: 2016-09-03 #$"    /* date version */
 
 #define BLOSCLZ_VERSION_STRING "1.0.5"   /* the internal compressor version */
 
