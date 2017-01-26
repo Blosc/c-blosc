057c0aff15689d2772c46d289c6970060ab295e7
Francesc Alted <faltet@gmail.com>
2017-01-26 13:47:36 +0100

    Limited the compression level in LZ4HC to 9 (other levels do not buy too much compression)

--- blosc/blosc.c
+++ blosc/blosc.c
@@ -419,9 +419,10 @@ static int lz4hc_wrap_compress(const char* input, size_t input_length,
   int cbytes;
   if (input_length > (size_t)(2<<30))
     return -1;   /* input larger than 1 GB is not supported */
-  /* clevel for lz4hc goes up to 16, at least in LZ4 1.1.3 */
+  /* clevel for lz4hc goes up to 12, at least in LZ4 1.7.5
+   * but levels larger than 9 does not buy much compression. */
   cbytes = LZ4_compressHC2_limitedOutput(input, output, (int)input_length,
-                                         (int)maxout, clevel*2-1);
+                                         (int)maxout, clevel);
   return cbytes;
 }
 
