690b02b2243ccdd1b1461cd854bd299fed3c7fa9
Francesc Alted <faltet@gmail.com>
2017-01-27 14:29:27 +0100

    Replace the deprecated LZ4HC compression function

--- blosc/blosc.c
+++ blosc/blosc.c
@@ -421,8 +421,8 @@ static int lz4hc_wrap_compress(const char* input, size_t input_length,
     return -1;   /* input larger than 1 GB is not supported */
   /* clevel for lz4hc goes up to 12, at least in LZ4 1.7.5
    * but levels larger than 9 does not buy much compression. */
-  cbytes = LZ4_compressHC2_limitedOutput(input, output, (int)input_length,
-                                         (int)maxout, clevel);
+  cbytes = LZ4_compress_HC(input, output, (int)input_length, (int)maxout,
+                           clevel);
   return cbytes;
 }
 
