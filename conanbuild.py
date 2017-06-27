a1543e1ef2d0ab203b0c1adde109d0a7f3ac3fc6
Francesc Alted <faltet@gmail.com>
2017-06-27 11:47:09 +0200

    Build only for a release

--- conanbuild.py
+++ conanbuild.py
@@ -13,4 +13,9 @@ def system(command):
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=True)
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["build_type"] == "Release":
+             filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
