44c3ecf05f6436c967857b9ee74f11245e5cf9b0
Francesc Alted <faltet@gmail.com>
2017-06-27 12:05:46 +0200

    Fixed the if condition in the Windows shell

--- appveyor/winrun.cmd
+++ appveyor/winrun.cmd
@@ -1,9 +1,18 @@
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (
-    cmake "-G%GENERATOR%" -H. -B_builds
-    cmake --build _builds --config "%CONFIG%"
-    cd _builds
-    ctest -VV -C "%CONFIG%"
-) ELSE (
-    SET CONAN_REFERENCE=c-blosc/%APPVEYOR_REPO_TAG_NAME%
-    python conanbuild.py
-)
+IF /i "%APPVEYOR_REPO_TAG_NAME%"=="" goto CMake
+IF /i NOT "%CONAN_VISUAL_VERSIONS%"=="" goto Conan
+goto commonexit
+
+:CMake
+cmake "-G%GENERATOR%" -H. -B_builds
+cmake --build _builds --config "%CONFIG%"
+cd _builds
+ctest -VV -C "%CONFIG%"
+goto commonexit
+
+:Conan
+SET CONAN_REFERENCE=c-blosc/%APPVEYOR_REPO_TAG_NAME%
+python "conanbuild.py"
+goto commonexit
+
+:commonexit
+exit
