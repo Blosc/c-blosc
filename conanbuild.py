c43dca80abc242065c979106e7ecbf630ba3e0fa
Francesc Alted <faltet@gmail.com>
2017-06-27 12:07:00 +0200

    Remove unnecessary function

--- conanbuild.py
+++ conanbuild.py
@@ -4,12 +4,6 @@ import platform
 from conan.packager import ConanMultiPackager
 
 
-def system(command):
-    retcode = os.system(command)
-    if retcode != 0:
-        raise Exception("Error while executing:\n\t %s" % command)
-
-
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=True)
