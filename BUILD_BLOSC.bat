0b232f2f52ef82548cd3cd1cd9d57fc04e050805
AntoineConffx <antoine@conffx.com>
2017-07-03 12:59:44 -0700

    Now compiling 64bit version of blosc. CMAKE now using VS2015 64bit compiler

--- BUILD_BLOSC.bat
+++ BUILD_BLOSC.bat
@@ -3,22 +3,24 @@ setlocal EnableDelayedExpansion
 :: Libraries from local folder
 @SETLOCAL
 @ECHO off
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64
 
 set OUT_DIR=.
+set BUILD_TYPE="Release"
 
 :: Remove existing Dll's
-if exist "%OUT_DIR%\blosc\Release" rmdir /s /q "%OUT_DIR%\blosc\Release"
+if exist "%OUT_DIR%\blosc\%BUILD_TYPE%" rmdir /s /q "%OUT_DIR%\blosc\%BUILD_TYPE%"
 if errorlevel 1 exit /b %errorlevel%
 
 ::Assume this script lives in root of blosc repo
 set "lastCompileAttempt=Blosc"
 ::create visual studio project with cmake
 @ECHO ON
-cmake -DCMAKE_INSTALL_PREFIX="%OUT_DIR%"
-cmake --build . --target blosc_shared --config Release
+cmake -DCMAKE_INSTALL_PREFIX="%OUT_DIR%" -G "Visual Studio 14 2015 Win64" .
+cmake --build . --target blosc_shared --config %BUILD_TYPE%
 @ECHO OFF
 if errorlevel 1 exit /b %errorlevel%
+
+::@ECHO ON
 
 ::remove all files used for building
 del /s /q /f "%OUT_DIR%\*.vcxproj"
@@ -33,13 +35,12 @@ del /s /q /f "%OUT_DIR%\blosc.sln"
 del /s /q /f "%OUT_DIR%\blosc\config.h"
 
 ::remove all folders used for building
-rmdir /s /q "%OUT_DIR%\CMakeFiles"
+rmdir /s /q "%OUT_DIR%\blosc\blosc_shared.dir"
+rmdir /s /q "%OUT_DIR%\x64"
 rmdir /s /q "%OUT_DIR%\bench\CMakeFiles"
 rmdir /s /q "%OUT_DIR%\blosc\CMakeFiles"
-rmdir /s /q "%OUT_DIR%\blosc\blosc_shared.dir"
-rmdir /s /q "%OUT_DIR%\Win32"
-rmdir /s /q "%OUT_DIR%\Release"
 rmdir /s /q "%OUT_DIR%\tests\CMakeFiles"
+rmdir /s /q "%OUT_DIR%\CMakeFiles"
 if errorlevel 1 exit /b %errorlevel%
 
 exit /b 0
