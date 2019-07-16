dfcd0e949a89bf4bdaab71fb0805625f7f7a284a
Francesc Alted <faltet@gmail.com>
2019-07-16 19:35:43 +0200

    BloscLZ codec updated to 2.0.0

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -25,7 +25,7 @@ extern "C" {
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
 #define BLOSC_VERSION_DATE     "$Date:: 2019-03-08 #$"    /* date version */
 
-#define BLOSCLZ_VERSION_STRING "1.1.0"   /* the internal compressor version */
+#define BLOSCLZ_VERSION_STRING "2.0.0"   /* the internal compressor version */
 
 /* The *_FORMAT symbols should be just 1-byte long */
 #define BLOSC_VERSION_FORMAT    2   /* Blosc format version, starting at 1 */
