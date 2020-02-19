2cf274faff0984130688c73eada7294e59714483
Francesc Alted <faltet@gmail.com>
2020-02-19 10:23:27 +0100

    Add another test for bitshuffle 4

--- tests/test_bitshuffle_leftovers.c
+++ tests/test_bitshuffle_leftovers.c
@@ -13,25 +13,15 @@
 
 #include "test_common.h"
 
-
-int main() {
-  /* `size` below is chosen so that it is not divisible by 8
-   * (not supported by bitshuffle) and in addition, it is not
-   * divisible by 8 (typesize) again.
-   */
-  int size = 641092;
-  int8_t *data = malloc(size);
-  int8_t *data_out = malloc(size + BLOSC_MIN_HEADER_LENGTH);
-  int8_t *data_dest = malloc(size);
-
-  for (int i = 0; i < size; i++) {
-    data[i] = i;
+static int test_roundtrip_bitshuffle8(int size, void *data, void *data_out, void *data_dest) {
+  /* Initialize data */
+  for (int i = 0; i < size / sizeof(int32_t); i++) {
+    ((uint32_t*)data)[i] = i;
   }
-
-  blosc_init();
-  blosc_set_nthreads(1);
-  blosc_set_compressor("lz4");
-  printf("Blosc version info: %s (%s)\n", BLOSC_VERSION_STRING, BLOSC_VERSION_DATE);
+  /* leftovers */
+  for (int i = size / sizeof(int32_t); i < size; i++) {
+    ((uint8_t*)data)[i] = i;
+  }
 
   /* Compress with bitshuffle active  */
   int isize = size;
@@ -46,10 +36,10 @@ int main() {
     return csize;
   }
 
-  printf("Compression: %d -> %d (%.1fx)\n", isize, csize, (1.*isize) / csize);
-  FILE *fout = fopen("test_bitshuffle_leftovers.data", "w");
-  fwrite(data_out, csize, 1, fout);
-  fclose(fout);
+//  printf("Compression: %d -> %d (%.1fx)\n", isize, csize, (1.*isize) / csize);
+//  FILE *fout = fopen("test_bitshuffle_leftovers.data", "w");
+//  fwrite(data_out, csize, 1, fout);
+//  fclose(fout);
 
   /* Decompress  */
   int dsize = blosc_decompress(data_out, data_dest, isize);
@@ -67,9 +57,86 @@ int main() {
   else
     printf("Decompressed data differs from original!");
 
+  return exit_code;
+}
+
+static int test_roundtrip_bitshuffle4(int size, void *data, void *data_out, void *data_dest) {
+  /* Initialize data */
+  for (int i = 0; i < size / sizeof(int32_t); i++) {
+    ((uint32_t*)data)[i] = i;
+  }
+  /* leftovers */
+  for (int i = size / sizeof(int32_t); i < size; i++) {
+    ((uint8_t*)data)[i] = i;
+  }
+
+  /* Compress with bitshuffle active  */
+  int isize = size;
+  int osize = size + BLOSC_MIN_HEADER_LENGTH;
+  int csize = blosc_compress(9, BLOSC_BITSHUFFLE, 4, isize, data, data_out, osize);
+  if (csize == 0) {
+    printf("Buffer is uncompressible.  Giving up.\n");
+    return 1;
+  }
+  else if (csize < 0) {
+    printf("Compression error.  Error code: %d\n", csize);
+    return csize;
+  }
+
+//  printf("Compression: %d -> %d (%.1fx)\n", isize, csize, (1.*isize) / csize);
+//  FILE *fout = fopen("test_bitshuffle_leftovers.data", "w");
+//  fwrite(data_out, csize, 1, fout);
+//  fclose(fout);
+
+  /* Decompress  */
+  int dsize = blosc_decompress(data_out, data_dest, isize);
+  if (dsize < 0) {
+    printf("Decompression error.  Error code: %d\n", dsize);
+    return dsize;
+  }
+
+  printf("Decompression succesful!\n");
+
+  int exit_code = memcmp(data, data_dest, size) ? EXIT_FAILURE : EXIT_SUCCESS;
+
+  if (exit_code == EXIT_SUCCESS)
+    printf("Succesful roundtrip!\n");
+  else
+    printf("Decompressed data differs from original!");
+
+  return exit_code;
+}
+
+int main() {
+  /* `size` below is chosen so that it is not divisible by 8
+   * (not supported by bitshuffle) and in addition, it is not
+   * divisible by 8 (typesize) again.
+   */
+  int size = 641092;
+  int32_t *data = malloc(size);
+  int32_t *data_out = malloc(size + BLOSC_MIN_HEADER_LENGTH);
+  int32_t *data_dest = malloc(size);
+  int result;
+
+  blosc_init();
+  blosc_set_nthreads(1);
+  blosc_set_compressor("lz4");
+  printf("Blosc version info: %s (%s)\n", BLOSC_VERSION_STRING, BLOSC_VERSION_DATE);
+  result = test_roundtrip_bitshuffle4(size, data, data_out, data_dest);
+  if (result != EXIT_SUCCESS) {
+    goto fail;
+  }
+  result = test_roundtrip_bitshuffle8(size, data, data_out, data_dest);
+  if (result != EXIT_SUCCESS) {
+    goto fail;
+  }
+
   free(data);
   free(data_out);
   free(data_dest);
 
-  return exit_code;
+  blosc_destroy();
+
+  fail:
+  return result;
 }
