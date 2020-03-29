ca416369a3f13fad7e1d0c4dc3198a97dfdcc84e
Francesc Alted <faltet@gmail.com>
2020-03-29 13:20:47 +0200

    Be compliant with MSVC 2015 limitations

--- tests/test_bitshuffle_leftovers.c
+++ tests/test_bitshuffle_leftovers.c
@@ -19,6 +19,10 @@ static int test_roundtrip_bitshuffle8(int size, void *data, void *data_out, void
   int isize = size;
   int osize = size + BLOSC_MIN_HEADER_LENGTH;
   int csize = blosc_compress(9, BLOSC_BITSHUFFLE, 8, isize, data, data_out, osize);
+  int dsize;
+  int exit_code;
+  FILE *fout = fopen("test-bitshuffle8-nomemcpy.cdata", "w");
+
   if (csize == 0) {
     printf("Buffer is uncompressible.  Giving up.\n");
     return 1;
@@ -29,12 +33,11 @@ static int test_roundtrip_bitshuffle8(int size, void *data, void *data_out, void
   }
   printf("Compression: %d -> %d (%.1fx)\n", isize, csize, (1.*isize) / csize);
 
-  FILE *fout = fopen("test-bitshuffle8-nomemcpy.cdata", "w");
   fwrite(data_out, csize, 1, fout);
   fclose(fout);
 
   /* Decompress  */
-  int dsize = blosc_decompress(data_out, data_dest, isize);
+  dsize = blosc_decompress(data_out, data_dest, isize);
   if (dsize < 0) {
     printf("Decompression error.  Error code: %d\n", dsize);
     return dsize;
@@ -42,7 +45,7 @@ static int test_roundtrip_bitshuffle8(int size, void *data, void *data_out, void
 
   printf("Decompression succesful!\n");
 
-  int exit_code = memcmp(data, data_dest, size) ? EXIT_FAILURE : EXIT_SUCCESS;
+  exit_code = memcmp(data, data_dest, size) ? EXIT_FAILURE : EXIT_SUCCESS;
 
   if (exit_code == EXIT_SUCCESS)
     printf("Succesful roundtrip!\n");
@@ -57,6 +60,10 @@ static int test_roundtrip_bitshuffle4(int size, void *data, void *data_out, void
   int isize = size;
   int osize = size + BLOSC_MIN_HEADER_LENGTH;
   int csize = blosc_compress(9, BLOSC_BITSHUFFLE, 4, isize, data, data_out, osize);
+  int dsize;
+  int exit_code;
+  FILE *fout = fopen("test-bitshuffle4-memcpy.cdata", "w");
+
   if (csize == 0) {
     printf("Buffer is uncompressible.  Giving up.\n");
     return 1;
@@ -67,12 +74,11 @@ static int test_roundtrip_bitshuffle4(int size, void *data, void *data_out, void
   }
   printf("Compression: %d -> %d (%.1fx)\n", isize, csize, (1.*isize) / csize);
 
-  FILE *fout = fopen("test-bitshuffle4-memcpy.cdata", "w");
   fwrite(data_out, csize, 1, fout);
   fclose(fout);
 
   /* Decompress  */
-  int dsize = blosc_decompress(data_out, data_dest, isize);
+  dsize = blosc_decompress(data_out, data_dest, isize);
   if (dsize < 0) {
     printf("Decompression error.  Error code: %d\n", dsize);
     return dsize;
@@ -80,8 +86,7 @@ static int test_roundtrip_bitshuffle4(int size, void *data, void *data_out, void
 
   printf("Decompression succesful!\n");
 
-  int exit_code = memcmp(data, data_dest, size) ? EXIT_FAILURE : EXIT_SUCCESS;
-
+  exit_code = memcmp(data, data_dest, size) ? EXIT_FAILURE : EXIT_SUCCESS;
   if (exit_code == EXIT_SUCCESS)
     printf("Succesful roundtrip!\n");
   else
@@ -100,13 +105,14 @@ int main() {
   int32_t *data_out = malloc(size + BLOSC_MIN_HEADER_LENGTH);
   int32_t *data_dest = malloc(size);
   int result;
+  int i;
 
   /* Initialize data */
-  for (int i = 0; i < size / sizeof(int32_t); i++) {
+  for (i = 0; i < size / sizeof(int32_t); i++) {
     ((uint32_t*)data)[i] = i;
   }
   /* leftovers */
-  for (int i = size / sizeof(int32_t) * sizeof(int32_t); i < size; i++) {
+  for (i = size / sizeof(int32_t) * sizeof(int32_t); i < size; i++) {
     ((uint8_t*)data)[i] = i;
   }
 
@@ -132,4 +138,3 @@ int main() {
   fail:
   return result;
 }
-
