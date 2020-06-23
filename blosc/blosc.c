790fac52990c73ebc217393879b49c7376607538
Francesc Alted <faltet@gmail.com>
2020-06-23 10:29:29 +0200

    Fix comment

--- blosc/blosc.c
+++ blosc/blosc.c
@@ -424,9 +424,9 @@ static int lz4hc_wrap_compress(const char* input, size_t input_length,
 {
   int cbytes;
   if (input_length > (size_t)(UINT32_C(2)<<30))
-    return -1;   /* input larger than 1 GB is not supported */
+    return -1;   /* input larger than 2 GB is not supported */
   /* clevel for lz4hc goes up to 12, at least in LZ4 1.7.5
-   * but levels larger than 9 does not buy much compression. */
+   * but levels larger than 9 do not buy much compression. */
   cbytes = LZ4_compress_HC(input, output, (int)input_length, (int)maxout,
                            clevel);
   return cbytes;
